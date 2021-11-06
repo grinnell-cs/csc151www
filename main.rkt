5f3e6bed45f7a89ec51adf8faef8ef5c0454ac0c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2021-11-06 12:37:56 -0500

    First attempt

---
+++ main.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+;;; File:
+;;;   csc151www/main.rkt
+;;; Summary:
+;;;   A combination of all procedures defined in the csc151 collection.
+;;; Authors:
+;;;   Samuel A. Rebelsky
+
+(require "www.rkt")
+
+(provide
+  (all-from-out "www.rkt"))
