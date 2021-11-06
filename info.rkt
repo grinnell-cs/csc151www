3ddb80a1ce0916c7b84cc8a6630feeef69b595b6
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2021-11-06 12:52:26 -0500

    Update info to remove scribblings.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,6 @@
 #lang info
 (define collection "csc151www")
 (define deps '("base" "html-parsing" "html-writing" "sxml"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/loudhum.scrbl" ())))
 (define pkg-desc "Code for the CSC151 www stuff")
 (define version "0.1.0")
 (define pkg-authors '(rebelsky))
